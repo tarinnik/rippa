2954bfda9e2221c6dde29dfc29f999d1eb15ffba
tk <git@tpk.net.au>
2026-04-22 17:13:21 +1000

    Test basic functionality

--- rippa/src/main.rs
+++ rippa/src/main.rs
@@ -1,7 +1,7 @@
 #![forbid(clippy::unwrap_used)]
 
 use crate::routes::get_router;
-use anyhow::Result;
+use makemkv::MakeMkv;
 use tokio::net::TcpListener;
 
 mod error;
@@ -11,16 +11,36 @@ mod templates;
 
 #[tokio::main]
 async fn main() {
+    if let Err(e) = run_makemkv().await {
+        eprintln!("MakeMKV error: {:#}", e);
+    }
+
     if let Err(e) = start_server().await {
         eprintln!("Server error: {}", e);
     }
 }
 
-async fn start_server() -> Result<()> {
+async fn start_server() -> anyhow::Result<()> {
     let router = get_router();
 
     let listener = TcpListener::bind("0.0.0.0:45566").await?;
     axum::serve(listener, router).await?;
+
+    Ok(())
+}
+
+async fn run_makemkv() -> anyhow::Result<()> {
+    let mut makemkv = MakeMkv::new();
+    println!("init");
+    makemkv.init().await?;
+    println!("set output folder");
+    makemkv.set_output_folder("/data/media/dmp").await?;
+    println!("wait for disc");
+    makemkv.wait_for_disc_inserted().await?;
+    println!("get disc data");
+    makemkv.get_disc_data().await?;
+
+    println!("Disc data: {:?}", &makemkv.titles);
 
     Ok(())
 }
